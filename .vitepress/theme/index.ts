80a36550fe952dc8c85c1cb92f96fcb3ecf10782
24mSearchlight <2493150890@qq.com>
2025-05-13 20:15:28 +0800

    feat: 添加音乐插件

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -25,30 +25,7 @@ import DataPanel from "./components/DataPanel.vue"
 import update from "./components/update.vue"
 import ArticleMetadata from "./components/ArticleMetadata.vue"
 
-const playlist = [
-    {
-        name: '永恒呼吸',
-        author: '塞壬唱片MSR',
-        file: 'https://24msearchlight.oss-cn-beijing.aliyuncs.com/music/%E6%B0%B8%E6%81%92%E5%91%BC%E5%90%B8%20-%20%E5%A1%9E%E5%A3%AC%E5%94%B1%E7%89%87-MSR%2CYU.flac',
-    },
-    {
-        name: '呼唤落花之名',
-        file: 'https://24msearchlight.oss-cn-beijing.aliyuncs.com/music/%E5%91%BC%E5%94%A4%E8%90%BD%E8%8A%B1%E4%B9%8B%E5%90%8D%20-%20MoreanP.mp3',
-
-    },
-    {
-        name: 'Believing',
-        file: 'https://24msearchlight.oss-cn-beijing.aliyuncs.com/music/%28Believed%20Believes%29%20Believing%20%28Instrumental%29%20-%20%E5%A1%9E%E5%A3%AC%E5%94%B1%E7%89%87-MSR%2CDavid%20Lin%2CErik%20Castro.flac',
-    },
-    {
-        name: 'PRAGMATISM',
-        file: 'https://24msearchlight.oss-cn-beijing.aliyuncs.com/music/PRAGMATISM%20-RESURRECTION-%20-%20Laur.flac',
-    },
-    {
-        name: 'Last',
-        file: 'https://24msearchlight.oss-cn-beijing.aliyuncs.com/music/Last%20-%20onoken.flac',
-    },
-]
+import {playlist} from "./untils/musiclist";
 
 
 export default {
